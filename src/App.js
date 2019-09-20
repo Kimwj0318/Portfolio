197ea8851a05256eebb6d138aa32a723e94db5c5
Andrew Kim <AndrewKim@Admin.local>
2019-09-20 13:07:07 -0600

    added projects to state

--- src/App.js
+++ src/App.js
@@ -11,10 +11,38 @@ const ABOUT = "About";
 function App() {
   const [state, setState] = useState({
     navOption: "Personal-info",
-    navOptions: ["Personal-info", "About"]
+    navOptions: ["Personal-info", "About"],
+    project: "",
+    projects: [
+      {
+        name: "TinyApp",
+        image: "source",
+        desc: "Full stack web application that allows users to shorten urls built wiht Node and Express",
+        github: "github"
+      },
+      {
+        name: "Tweeter",
+        image: "source",
+        desc: "A single page twitter clone implementing AJAX JQuery, CSS, and HTML",
+        github: "github"
+      },
+      {
+        name: "Pick-Me-Up",
+        image: "source",
+        desc: "Online pick me up app replica developed with a Lighthouse Labs cohort member that allows users to place an order online which sends the 'client' and 'restaurant owner' a text using JS, CSS, EJS, PostgreSQL, JQuery, and twilio",
+        github: "github"
+      },
+      {
+        name: "Interview Scheduler",
+        image: "source",
+        desc: "Full stack web application built with React. Allows users to create, edit, and delete interviews in the interview planner developed using ReactJS, PostgreSQL, Jest, Cypress, and Storybook",
+        github: "github"
+      }
+    ]
   })
 
   const setNav = (navOption) => setState({...state, navOption})
+  const setProject = (project) => setState({...state, project})
 
   return (
     <div className="App">
@@ -35,10 +63,13 @@ function App() {
           )}
         </section>
         <section className="project">
-          <Projects />
+          <Projects 
+            selected={state.project}
+            projectOptions={state.projects}
+            setProject={setProject}
+          />
         </section>
       </div>
-      
     </div>
   );
 }
